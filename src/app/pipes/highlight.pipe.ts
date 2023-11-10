76c7da6d569c621169a963c5521f27988ecff894
Al Ameen AR <amrameen769@gmail.com>
2023-11-10 12:23:55 +0530

    tweaked highlight pipe

--- src/app/pipes/highlight.pipe.ts
+++ src/app/pipes/highlight.pipe.ts
@@ -8,6 +8,6 @@ export class HighlightPipe implements PipeTransform {
   transform(value: any, args: any): any {
     if (!args) { return value; }
     var re = new RegExp(args, 'gi'); //'gi' for case insensitive and can use 'g' if you want the search to be case sensitive.
-    return value.replace(re, `<span>$&</span>`);
+    return value.replace(re, `<span class='highlight'>$&</span>`);
   }
 }
