b1161f03e7d9326a6b74f1b995619f197548ea35
Al Ameen AR <amrameen769@gmail.com>
2023-11-10 12:18:49 +0530

    created highlight directive

--- src/app/pipes/highlight.pipe.ts
+++ src/app/pipes/highlight.pipe.ts
@@ -1,13 +1,13 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'highlight'
+  name: 'highlight',
 })
 export class HighlightPipe implements PipeTransform {
 
   transform(value: any, args: any): any {
     if (!args) { return value; }
     var re = new RegExp(args, 'gi'); //'gi' for case insensitive and can use 'g' if you want the search to be case sensitive.
-    return value.replace(re, `<strong>$&</strong>`);
+    return value.replace(re, `<span>$&</span>`);
   }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HighlightPipe } from '../pipes/highlight.pipe';
 import { BreadcrumpsComponent } from '../breadcrumps/breadcrumps.component';
+import { SearchHighlightDirective } from './directives/search-highlight/search-highlight.directive';
 
 
 
 @NgModule({
   declarations: [
     HighlightPipe,
-    BreadcrumpsComponent
+    BreadcrumpsComponent,
+    SearchHighlightDirective
   ],
   imports: [
     CommonModule
   ],
   exports: [
     HighlightPipe,
-    BreadcrumpsComponent
+    BreadcrumpsComponent,
+    SearchHighlightDirective
   ]
 })
 export class SharedModule { }
