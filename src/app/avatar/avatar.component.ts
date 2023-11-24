4f6dceb6b4549e09c2f56ceaab8df9f038ce79c5
Al Ameen AR <amrameen769@gmail.com>
2023-11-24 20:38:11 +0530

    added transform animation

--- src/app/avatar/avatar.component.ts
+++ src/app/avatar/avatar.component.ts
@@ -9,16 +9,40 @@ import {
 
 const DropDownAnimation = trigger("dropDownMenu", [
   transition(":enter", [
-    style({ opacity: 0 }),
+    style({
+      opacity: 0,
+      position: 'absolute',
+      top: '4.25rem',
+      left: '-13.5rem',
+      transform: 'scaleY(0)'
+    }),
     sequence([
-      animate("200ms", style({ opacity: 1 })),
+      animate("200ms", style({
+        opacity: 1,
+        position: 'absolute',
+        top: '4.25rem',
+        left: '-13.5rem',
+        transform: 'scaleY(100%)'
+      })),
     ])
   ]),
 
   transition(":leave", [
-    style({ opacity: 1 }),
+    style({
+      opacity: 1,
+      position: 'absolute',
+      top: '4.25rem',
+      left: '-13.5rem',
+      transform: 'scaleY(100%)'
+    }),
     sequence([
-      animate("200ms", style({opacity: 0 }))
+      animate("200ms", style({
+        opacity: 0,
+        position: 'absolute',
+        top: '4.25rem',
+        left: '-13.5rem',
+        transform: 'scaleY(0)'
+      }))
     ])
   ])
 ]);
