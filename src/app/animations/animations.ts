4f1c4b3837149b3c87a8eb4c103cdba86a647139
Al Ameen AR <amrameen769@gmail.com>
2023-11-24 20:45:08 +0530

    moved animation to separate file

---
+++ src/app/animations/animations.ts
@@ -0,0 +1,47 @@
+import {
+    trigger,
+    style,
+    animate,
+    transition,
+    sequence,
+  } from '@angular/animations';
+  
+  export const DropDownAnimation = trigger("dropDownMenu", [
+    transition(":enter", [
+      style({
+        opacity: 0,
+        position: 'absolute',
+        top: '1rem',
+        left: '-13.5rem',
+        transform: 'scaleY(0)'
+      }),
+      sequence([
+        animate("200ms", style({
+          opacity: 1,
+          position: 'absolute',
+          top: '4.25rem',
+          left: '-13.5rem',
+          transform: 'scaleY(100%)'
+        })),
+      ])
+    ]),
+  
+    transition(":leave", [
+      style({
+        opacity: 1,
+        position: 'absolute',
+        top: '4.25rem',
+        left: '-13.5rem',
+        transform: 'scaleY(100%)'
+      }),
+      sequence([
+        animate("200ms", style({
+          opacity: 0,
+          position: 'absolute',
+          top: '1rem',
+          left: '-13.5rem',
+          transform: 'scaleY(0)'
+        }))
+      ])
+    ])
+  ]);
